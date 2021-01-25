16d8689f602c4a0955e5a9d85ee0be26d93d4155
XabierArrieta <xabierarrietachango@gmail.com>
2021-01-26 00:55:38 +0100

    Proyecto Pandas

--- src/cleaning_func.py
+++ src/cleaning_func.py
@@ -1,9 +1,10 @@
+import numpy as np
+
 #Calculo el porcentaje de hombres y mujeres.
 def porcentaje(i,j):
     male = round((i * 100)/(i+j))
     female = round((j * 100)/(i+j))
     return (male, female)
-
 
 
 #En las edades quiero que coja sólo dos valores.
@@ -18,3 +19,29 @@ def ages(i):
 
     except:
         return np.nan
+
+#Agrupo las actividades por las que más ataques tienen.
+def activity(x):
+    if x == "Surfing": 
+        return "Surfing"
+    elif x == "Swimming": 
+        return "Swimming"
+    elif x == "Wading": 
+        return "Wading"
+    elif x == "Standing": 
+        return "Standing"
+    elif x == "Fishing": 
+        return "Fishing"
+    else:
+        return "Other"
+        
+#Agrupo los nombres y los que no son.
+def name(x):
+    if x == "male": 
+        return "NoName"
+    elif x == "female": 
+        return "NoName"
+    elif x == "girl": 
+        return "NoName"
+    else:
+        return x
